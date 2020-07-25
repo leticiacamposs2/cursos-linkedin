a45befa02370665fec538d3b769ec08ca2d5292e
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-07-25 13:29:18 -0300

    tipo de dados

---
+++ javascript-formacao-basica/manipulando-dados/script.js
@@ -0,0 +1,25 @@
+var a = 5;
+var b = 4;
+var sum = a + b;
+var emptyInside = null;
+var justAnotherVariable;
+
+// Armazena números
+
+var number = 1;
+var integer = 3.14159265359;
+var negNumber = -1;
+var negInteger = -3.14159265359;
+
+// Armazena strings de letras e símbolos:
+
+var string = "Linhas geralmente são palavras e sentenças.";
+var mixedQuote = 'Isso mantém as "aspas".';
+var escQuote = "Citações também podem ser \"assim\".";
+
+// Armazena um ou o outro do par binário verdadeiro/falso:
+
+var theSunIsWarm = true;
+var theMoonIsMadeOfCheese = false;
+
+
